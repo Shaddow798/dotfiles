afc2ef1a1e7b99bab8cffa4f192337ef7da07ce0
Shaddow798 <samorange370@gmail.com>
2023-04-18 12:08:09 +1200

    Make the script more idiot proff and add packages

--- install.sh
+++ install.sh
@@ -2,13 +2,29 @@
 
 arch_runner () {
 	sh packages.sh
-	echo Done
+	echo Packages completed
 }
+
+echo This script will configure archlinux
+echo This script will modify your system in ways that cannot easily be undone
+echo This script comes with zero warrenty use at your own risk
+echo 
+echo
+
+
+# Warn user then ask for consent
+read -p "Are you sure? [N/y]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	exit 1
+fi
+
 
 
 # Check to make sure the user is running a supported operating system.
 if [ -f "/etc/arch-release" ]; then
-	echo You are running Arch Linux do you want to continue,
+	echo System is compatiable continuing
 	arch_runner
 else
  echo Error unsuported operating system.
--- packages.sh
+++ packages.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
-echo Installing packages
-package_list=(firefox vim nano)
-for package in package_list do
-	if pacman -Qs $package > /dev/null ; then
-		echo "The package $package is installed"
-	else
-  		echo "The package $package is not installed"
-fi
+echo Making sure system is uptodate
 
+sudo pacman -Syu --noconfirm
+
+echo Installing packages
+package_list=(vim base-devel git xorg lightdm lightdm-gtk-greeter networkmanager i3 dmenu thunar papirus-icon-theme adapta-gtk-theme)
+for package in ${package_list[@]}; do
+	if pacman -Qs $package > /dev/null ; then
+		# echo "The package $package is installed"
+		echo > /dev/null
+	else
+  		# echo "The package $package is not installed"
+		packages_install+=($package)
+	fi
+done
+echo Installing needed packages
+sudo pacman -S --needed ${packages_install[@]};
